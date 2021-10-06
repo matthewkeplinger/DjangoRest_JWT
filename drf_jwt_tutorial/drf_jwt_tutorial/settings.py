bbe3f9fabf470d192c46f01a2c47fe4e1c8d4043
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-06 09:50:53 -0400

    Created Cars App, Created Car Model, Registered Car on Admin site, Migrated Car to DB

--- drf_jwt_tutorial/drf_jwt_tutorial/settings.py
+++ drf_jwt_tutorial/drf_jwt_tutorial/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'authentication.apps.AuthenticationConfig'
+    'authentication.apps.AuthenticationConfig',
+    'cars.apps.CarsConfig'
 ]
 
 MIDDLEWARE = [
