e47a6a039fca20b40d5db4627e7aaa22cf02b964
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-10-06 09:45:06 -0400

    Created Authentication App URLS file, Registered on project URLS

--- drf_jwt_tutorial/authentication/serializers.py
+++ drf_jwt_tutorial/authentication/serializers.py
@@ -4,7 +4,7 @@ from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required = True, validators[UniqueValidator(queryset = User.objects.all())])
+    email = serializers.EmailField(required = True, validators=[UniqueValidator(queryset = User.objects.all())])
     password = serializers.CharField(write_only= True, required=True, validators=[validate_password])
 
     class Meta:
